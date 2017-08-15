2e637bed21b15e7be9d6bb9d32206cab61d066cd
Ka77y <geomitaviva@hotmail.com>
2017-08-14 23:13:15 -0500

    creado usuario y login

---
+++ aplicacion/api/controllers/UsuarioController.js
@@ -0,0 +1,79 @@
+/**
+ * Created by USRDEL on 19/6/17.
+ */
+
+module.exports = {
+  welcome: function (req, res) {
+    sails.log.info(req.method);
+    if (req.method == "POST") {
+      return res.json({ saludo: "hola" });
+    }
+    else {
+      return res.send("Error");
+    }
+  },
+  bienvenido: function (req, res) {
+
+    return res.send("Hola");
+  },
+  crearUsuarioQuemado: function (req, res) {
+    var parametros = req.allParams();
+    sails.log.info("Parametros", parametros);
+    var nuevoUsuario = {
+      nombres: parametros.nombres,
+      apellidos: parametros.apellidos,
+      password: parametros.password,
+      correo: parametros.correo,
+      fechaNacimiento: parametros.fechaNacimiento
+    };
+
+    Usuario.create(nuevoUsuario)
+      .exec(function (error, usuarioCreado) {
+        if (error) {
+          return res.serverError(error);
+        }
+        else {
+          return res.redirect("/");
+
+        }
+      });
+  },
+  crearUsuario: function (req, res) {
+    return res.view('crearusuario');
+  },
+  login: function (req, res) {
+
+    var nombres = req.param('nombres');
+    var password = req.param('password');
+
+      Usuario.find({
+        nombres: nombres,
+        password: password // TODO: hash the password first
+      }).exec(function (err, user) {
+
+        if (err || !user) {
+          res.send("credenciales invalidas nombre o password",500);
+          // TODO: redirect, storing an error in the session
+        }
+
+        // Login succeeded
+        if (user) {
+          req.session.authenticated = true;
+          req.session.User = user;
+
+          // Redirect to protected area
+          res.redirect('https://www.facebook.com');
+        }
+
+    });
+  },
+  encontrarUsuario: function (req, res) {
+    Usuario.find({nombres:'Katherine'}).exec(function (err, usersNamedFinn){
+      if (err) {
+        return res.serverError(err);
+      }
+      sails.log('Wow, there are %d users named Finn.  Check it out:', usersNamedFinn.length, usersNamedFinn);
+      return res.json(usersNamedFinn);
+    });
+  },
+};
---
+++ aplicacion/api/models/Usuario.js
@@ -0,0 +1,13 @@
+/**
+ * Created by USRDEL on 26/6/17.
+ */
+module.exports = {
+  attributes: {
+    nombres: {
+      type: "string"
+    },
+    password: {
+      type: "string"
+    }
+  }
+};
